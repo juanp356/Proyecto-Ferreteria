887169be689b1cc5b799f24cb36a463fe6315f6e
Tatiana Carvajal <carvajaltatiana662@gmail.com>
2025-07-31 23:02:52 -0500

    se creo el back de inventario
    se creo el model de inventario con sus respectivas características

---
+++ Ferreteria/src/main/java/Model/Inventory.java
@@ -0,0 +1,66 @@
+package Model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "supplier")
+
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String name;
+    private String category;
+    private String contact;
+    private String price;
+    private String products_supplied;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getProducts_supplied() {
+        return products_supplied;
+    }
+
+    public void setProducts_supplied(String products_supplied) {
+        this.products_supplied = products_supplied;
+    }
+}
