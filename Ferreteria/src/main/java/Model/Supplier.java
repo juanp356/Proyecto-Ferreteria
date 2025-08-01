a94d9f744dc76ca3226e35a4ef4d9caebbb00968
Tatiana Carvajal <carvajaltatiana662@gmail.com>
2025-07-31 21:08:46 -0500

    Se creo el back de proveedor
    se agrego el model de proveedor

---
+++ Ferreteria/src/main/java/Model/Supplier.java
@@ -0,0 +1,47 @@
+package Model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "supplier")
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String name;
+    private String charge;
+    private String salary;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharge() {
+        return charge;
+    }
+
+    public void setCharge(String charge) {
+        this.charge = charge;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+}
