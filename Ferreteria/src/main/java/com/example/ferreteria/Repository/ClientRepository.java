3865ec95f1f582097f5a00134942706ce6aa7bb0
Juan Giraldo <juanpablogiraldo367@gmail.com>
2025-08-12 18:41:21 -0500

    Fixes
    se hizo corrección de código en varios módulos

--- Ferreteria/src/main/java/com/example/ferreteria/Repository/ClientRepository.java
+++ Ferreteria/src/main/java/com/example/ferreteria/Repository/ClientRepository.java
@@ -2,6 +2,15 @@ package com.example.ferreteria.Repository;
 
 import com.example.ferreteria.Model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    @Query("SELECT COUNT(c) FROM Client c")
+    Long countClients();
+
+    @Query("SELECT COUNT(c) FROM Client c")
+    Long countClientsR();
+
+
 }
--- Ferreteria/src/main/java/com/example/ferreteria/Repository/ProceedsRepository.java
+++ Ferreteria/src/main/java/com/example/ferreteria/Repository/ProceedsRepository.java
@@ -2,6 +2,10 @@ package com.example.ferreteria.Repository;
 
 import com.example.ferreteria.Model.Proceeds;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProceedsRepository extends JpaRepository<Proceeds,Long>  {
+
+    @Query("SELECT COUNT(c) FROM Proceeds c")
+    Long countProceeds();
 }
